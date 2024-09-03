6e327bd4e48f62d646c178ffb793e3741ae636de
Leo-guerrero <sja.leo.guerrero@gmail.com>
2024-09-03 12:18:40 -0500

    Add files via upload

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Leonardo Guerrero on 9/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func btn_welcome(_ sender: UIButton) 
+    {
+        let alertController = UIAlertController(title: "Welcome User!", message: "Hello There!", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+}
+
